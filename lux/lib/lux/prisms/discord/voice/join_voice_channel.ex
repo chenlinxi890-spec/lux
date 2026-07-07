becbc7b669ea17dde8db521e1c7b0fbbd26e80b6
chenlinxi890-spec <chenlinxi890-spec@users.noreply.github.com>
2026-07-08 01:46:39 +0800

    feat(discord): add voice channel join/leave and rich presence prisms (#56)

---
+++ lux/lib/lux/prisms/discord/voice/join_voice_channel.ex
@@ -0,0 +1,19 @@
+﻿defmodule Lux.Prisms.Discord.VoiceChannelJoin do
+  @moduledoc """
+  Join a voice channel in a Discord guild.
+
+  Uses the Discord API to join a specified voice channel.
+  Requires bot to have Connect permission in the channel.
+  """
+
+  @spec join_voice_channel(Lux.Lens.t(), binary(), binary()) :: {:ok, map} | {:error, binary()}
+  def join_voice_channel(%Lux.Lens{} = lens, guild_id, channel_id) do
+    url = "https://discord.com/api/v10/guilds/#{guild_id}/voice-states/@me"
+
+    lens
+    |> Lux.Lens.put(:method, :patch)
+    |> Lux.Lens.put(:url, url)
+    |> Lux.Lens.put(:body, %{"channel_id" => channel_id})
+    |> Lux.Lens.execute()
+  end
+end
